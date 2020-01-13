f08ba81e4ab920949fe98abc51f9bb420ce24061
kmmajidh <kmmajidh@gmail.com>
2020-01-13 12:14:29 +0530

    realised directory content doesnot remain same, so used mock and test passed of get_mp3

--- test_audio_classifier.py
+++ test_audio_classifier.py
@@ -1,15 +1,30 @@
 from audio_classifier import get_mp3
+import unittest
+from unittest import mock
 
-def test_get_mp3_no_mp3():
+class Test_get_mp3(unittest.TestCase):
 
-    expected = []
-    actual = get_mp3("/home/majid")
+    def test_get_mp3(self):
+        with mock.patch('audio_classifier.listdir') as mocked_listdir:
+            mocked_listdir.return_value = ['1.mp3','2.mp3','3.mp3','4','n.py']
+            mp3_list = get_mp3(".")
+            self.assertEqual(len(mp3_list),3)
 
-    assert actual == expected
 
-def test_get_mp3_with_mp3():
+        
+        
 
-    expected = ['Oasis - Wonderwall (Official Video).mp3', 'Stand By Me, Ben E King, 1961.mp3', "Bob Dylan - Blowin' in the Wind (Audio).mp3"]
-    actual = get_mp3(".")
+# def test_get_mp3_no_mp3():
 
-    assert actual == expected
+#     expected = []
+#     actual = get_mp3("/home/majid")
+
+#     assert actual == expected
+
+# def test_get_mp3_with_mp3():
+
+#     expected = ['Oasis - Wonderwall (Official Video).mp3', 'Stand By Me, Ben E King, 1961.mp3', "Bob Dylan - Blowin' in the Wind (Audio).mp3"]
+#     actual = get_mp3(".")
+
+#     assert actual == expected
+    
