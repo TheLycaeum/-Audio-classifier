391b3d25ce10d37474b79773425f914ad6660045
kmmajidh <kmmajidh@gmail.com>
2020-01-14 10:40:53 +0530

    file details fetched from acoustid, no test done, tried with print

--- audio_classifier.py
+++ audio_classifier.py
@@ -1,10 +1,24 @@
 from os import listdir
+from key import api_key
+from acoustid import fingerprint_file, lookup
+
 def get_mp3(path):
 
-    mp3 = []
+    mp3_list = []
 
-    for file in listdir(path):
-        if ".mp3" in file:
-            mp3.append(file)
+    for files in listdir(path):
+        if ".mp3" in files:
+            mp3_list.append(files)
 
-    return mp3
+    return mp3_list
+
+def get_file_details(files):
+    duration, fingerprint = fingerprint_file(files)
+    file_details =  lookup(api_key, fingerprint, duration)
+    return file_details
+    
+if __name__ == "__main__":
+
+    mp3_list = get_mp3(".")
+    for files in mp3_list:
+        file_details = get_file_details(files)
--- test_audio_classifier.py
+++ test_audio_classifier.py
@@ -6,10 +6,16 @@ class Test_get_mp3(unittest.TestCase):
 
     def test_get_mp3(self):
         with mock.patch('audio_classifier.listdir') as mocked_listdir:
-            mocked_listdir.return_value = ['1.mp3','2.mp3','3.mp3','4','n.py']
+            mocked_listdir.return_value = ['1.py','2.mp3','3.mp3','4.mp3','n.py']
             mp3_list = get_mp3(".")
             self.assertEqual(len(mp3_list),3)
 
+
+    def test_get_mp3_no_mp3(self):
+        with mock.patch('audio_classifier.listdir') as mocked_listdir:
+            mocked_listdir.return_value = ['1.py','2.py','3.txt','4','n.py']
+            mp3_list = get_mp3(".")
+            self.assertEqual(len(mp3_list),0)
 
         
         
